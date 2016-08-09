a65d4c2b87b69f1e106247bd32b73b9720ed43e3
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-08-09 15:04:03 +0200

    add pretty formatting that align to 8 bytes

--- cpp_packet_parser.py
+++ cpp_packet_parser.py
@@ -1,21 +1,46 @@
+def packet_bytes(str):
+    number_of = 0
+    for x in str:
+        if x == '|':
+            number_of += 1;
+    return number_of
 
-num = 0
-hex_start = True
-str = "|00|ff|17|af|08|a4|fc|00|00|00|00|00|86|dd|60|00|00|00|00|28|3a|ff|fc|42|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|fc|9d|e6|d3|bd|5b|2b|c4|13|74|0d|1a|ce|bc|22|4d|88|00|28|9d|e0|00|00|00|fc|42|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|02|01|fc|00|00|00|00|00|01|01|00|ff|17|af|08|a4"
+def digits(num):
+    if num < 0:
+        return 0
+    digits = 1
+    while num > 10:
+        num /= 10
+        digits += 1
+    return digits
 
-for x in str:
-    if x == '|':
-        print("pack[", num, "] = ",sep="",end="")
-        num +=1
-        hex_start = True
-    else:
-        if hex_start:
-            print("0x",x,sep="",end="")
-            hex_start = False
-        else:
-            print(x,";",sep="",end=" ")
+if __name__ == "__main__":
+
+    num = 0
+    hex_start = True
+    str = input("Paste packet in hex format (e.g. from wireshark)  \"|3d|f5|23|fg|\" :\n")
+
+    # EXAMPLES OF INPUTS
+        #str = "|00|ff|17|af|08|a4|fc|00|00|00|00|00|86|dd|60|00|00|00|00|28|3a|ff|fc|42|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|fc|9d|e6|d3|bd|5b|2b|c4|13|74|0d|1a|ce|bc|22|4d|88|00|28|9d|e0|00|00|00|fc|42|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|02|01|fc|00|00|00|00|00|01|01|00|ff|17|af|08|a4"
+        #str = "|00|ff|17|af|08|a4|fc|00|00|00|00|00|86|dd|60|00|00|00|00|28|3a|ff|fc|00|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|fc|9d|e6|d3|bd|5b|2b|c4|13|74|0d|1a|ce|bc|22|4d|88|00|29|21|e0|00|00|00|fc|00|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|02|01|fc|00|00|00|00|00|01|01|00|ff|17|af|08|a4"
+
+    number_of_bytes = packet_bytes(str);
+
+    print(number_of_bytes);
+
+    for x in str:
+        if x == '|':
+            print("pack3[", num, "] = ",sep="",end="")
+            num +=1
             hex_start = True
+        else:
+            if hex_start:
+                print("0x",x,sep="",end="")
+                hex_start = False
+            else:
+                print(x,(digits(number_of_bytes)-digits(num))*" ",";",sep="",end=" ")
+                hex_start = True
 
-        if not (num) % 10 and hex_start:
-            print("")
+            if not (num) % 8 and hex_start:
+                print("")
 
