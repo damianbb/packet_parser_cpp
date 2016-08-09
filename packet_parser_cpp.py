9aae12c71bf329606f1fdd4afd923942621ca9bc
DamianB <01e9c647884e4a09085dc5e3258278c7@protonmail.com>
2016-08-09 15:28:15 +0200

    remove examples str from script - rename script

--- packet_parser_cpp.py
+++ packet_parser_cpp.py
@@ -18,11 +18,7 @@ if __name__ == "__main__":
 
     num = 0
     hex_start = True
-    str = input("Paste packet in hex format (e.g. from wireshark)  \"|3d|f5|23|fg|\" :\n")
-
-    # EXAMPLES OF INPUTS
-        #str = "|00|ff|17|af|08|a4|fc|00|00|00|00|00|86|dd|60|00|00|00|00|28|3a|ff|fc|42|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|fc|9d|e6|d3|bd|5b|2b|c4|13|74|0d|1a|ce|bc|22|4d|88|00|28|9d|e0|00|00|00|fc|42|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|02|01|fc|00|00|00|00|00|01|01|00|ff|17|af|08|a4"
-        #str = "|00|ff|17|af|08|a4|fc|00|00|00|00|00|86|dd|60|00|00|00|00|28|3a|ff|fc|00|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|fc|9d|e6|d3|bd|5b|2b|c4|13|74|0d|1a|ce|bc|22|4d|88|00|29|21|e0|00|00|00|fc|00|c6|b0|a9|06|85|96|4f|47|11|12|ec|ff|f9|55|02|01|fc|00|00|00|00|00|01|01|00|ff|17|af|08|a4"
+    str = input("Paste packet in hex format (e.g. from wireshark)  \"|3d|f5|23|fg\" :\n")
 
     number_of_bytes = packet_bytes(str);
 
